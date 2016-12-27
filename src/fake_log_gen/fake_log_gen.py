28672a979783f4be3e7b84d5a42b707b1724cef2
xuwenyihust <1016635786@qq.com>
2016-12-27 12:33:31 -0800

    Some updates.

--- src/fake_log_gen/fake_log_gen.py
+++ src/fake_log_gen/fake_log_gen.py
@@ -1,12 +1,4 @@
 # Fake log files generator
-#
-# Usage:
-#	python 3.4 fake_log_gen [fake_logfile]
-#
-# Format:
-#
-#  Error log:
-#	[Wed Oct 11 14:32:52 2000] [error] [client 127.0.0.1] client denied by server configuration: /export/home/live/ap/htdocs/test
 
 
 import random
@@ -14,43 +6,99 @@ import json
 import logging
 import argparse
 import asyncio
+import datetime
 from asyncio import coroutine
 
 
-class fake_log_gen():
-	
-	def __init__(self, log, config, log_type):
+class fake_log_gen(object):
+
+	def __init__(self, log, config, mode):
 		self.log = log
-		self.log_type = log_type
+		self.mode = mode
 		# Dict that contains config info
 		self.config = config
 
+	def run(self):
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(
+			asyncio.wait([
+				self.heartbeat_lines()] 
+			)
+		)
+		loop.close()
+
+
+class fake_access_gen(fake_log_gen):
+
+	def run(self):
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(
+			asyncio.wait([
+				self.heartbeat_lines(),
+				self.access_lines()]
+			)
+		)
+		loop.close()
+
+
+	@coroutine
+	def heartbeat_lines(self):
+		while True:
+			t = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S -0700')	
+			self.log.info('- - - [%s] "%s" - -', t, self.config["heartbeat"]["message"])
+			yield from asyncio.sleep(int(self.config["heartbeat"]["interval"]))
+
+	@coroutine
+	def access_lines(self):
+
+		access_min = self.config["access"]["interval"]["min"]
+		access_max = self.config["access"]["interval"]["max"]
+
+		user_ids = self.config["access"]["user_id"]
+		msgs = self.config["access"]["message"]
+
+		while True:
+			ip = '.'.join(str(random.randint(0, 255)) for i in range(4))
+			user_identifier = 'user-identifier'
+			user_id = user_ids[random.randint(0,len(user_ids)-1)]
+			t = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S -0700')
+			msg = msgs[random.randint(0, len(msgs)-1)]
+			self.log.info('%s %s %s [%s] "%s"', ip, user_identifier, user_id, t, msg)
+			yield from asyncio.sleep(random.uniform(access_min, access_max))
+
+
+class fake_error_gen(fake_log_gen):
+	
 	def run(self): 
 		loop = asyncio.get_event_loop()
 		# The event loop
-		if self.log_type == 'error':
-			loop.run_until_complete(
-				asyncio.wait([
-					self.heartbeat_lines(),
-					self.warn_lines(),
-					self.error_lines()]
-				)
+		loop.run_until_complete(
+			asyncio.wait([
+				self.heartbeat_lines(),
+				self.warn_lines(),
+				self.error_lines()]
 			)
-			loop.close()
-		else:
-			loop.run_until_complete(
-				asyncio.wait([
-					self.heartbeat_lines()]
-				)
-			)
-			loop.close()
-
+		)
+		loop.close()
 
 	@coroutine
 	def heartbeat_lines(self):
 		while True:
 			self.log.info("[-] [-] " + self.config["heartbeat"]["message"])
 			yield from asyncio.sleep(int(self.config["heartbeat"]["interval"]))
+
+	@coroutine
+	def access_lines(self):
+
+		access_min = self.config["access"]["interval"]["min"]
+		access_max = self.config["access"]["interval"]["max"]		
+
+		while True:
+			ip = '.'.join(str(random.randint(0, 255)) for i in range(4))
+			#user_identifier = 
+			#user_id =
+			self.log.info("%s", ip)
+			yield from asyncio.sleep(random.uniform(access_min, access_max)) 
 	
 	@coroutine
 	def warn_lines(self):
@@ -83,12 +131,12 @@ class fake_log_gen():
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("fake_logfile", help="fake logfile")
-	parser.add_argument("form", help="log format")
+	parser.add_argument("mode", help="log mode")
 	args = parser.parse_args()
 
 	# Identify the log format
-	log_type = args.form
-	if log_type not in ['error', 'access']:
+	mode = args.mode
+	if mode not in ['error', 'access']:
 		print('Argument error.')
 
 	# Instantiate the logger
@@ -99,7 +147,10 @@ def main():
 	out = logging.FileHandler(args.fake_logfile)
 	# Instantiate a Formatter
 	# Format the time string
-	log_format = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", "%a %b %d %H:%M:%S %Y")
+	if mode == 'error':
+		log_format = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", "%a %b %d %H:%M:%S %Y")
+	else:
+		log_format = logging.Formatter("%(message)s")
 	# Set the Formatter for this Handler to form
 	out.setFormatter(log_format)
 	# Add the file Handler 'out' to the logger'log'
@@ -115,7 +166,10 @@ def main():
 		config = json.load(config_file)
 
 	# Instantiate a fake log generator
-	log_gen = fake_log_gen(log, config, log_type)
+	if mode == 'error':
+		log_gen = fake_error_gen(log, config, mode)
+	else:
+		log_gen = fake_access_gen(log, config, mode)
 	log_gen.run()
 
 
