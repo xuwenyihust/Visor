9a33bd7b9d7c602a50002f45f07287338d65ca1c
xuwenyihust <1016635786@qq.com>
2017-01-15 11:56:10 -0800

    Log file test skeleton.

--- tests/generator_test.py
+++ tests/generator_test.py
@@ -5,6 +5,7 @@ import asyncio
 from asyncio import coroutine
 import numpy
 import datetime
+import json
 from src.fake_log_gen import fake_log_gen
 
 class access_test(fake_log_gen.fake_access_gen):
@@ -43,42 +44,42 @@ class access_test(fake_log_gen.fake_access_gen):
 
 	@coroutine
 	def heartbeat_lines(self):
-		for i in range(heart_num):
+		for i in range(self.heart_num):
 			t = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S -0700')
 			self.log.info('- - - [%s] "%s" - -', t, self.config["heartbeat"]["message"])
 			yield from asyncio.sleep(int(self.config["heartbeat"]["interval"]))
 
-class Log_Gen_Test_Class:
 
-	def __init__(self):
-		self.access_file = tmpdir.mkdir("sub").join("access_logs.txt")
-		self.error_file = tmpdir.mkdir("sub").join("error_logs.txt")
 
-		# Logs
-		self.log = logging.getLogger('Gen')
-		logging.basicConfig(level=logging.INFO)
-
-		log_format = logging.Formatter("%(message)s")
-		#out = logging.FileHandler('sub/access_logs.txt')
-		self.out = logging.FileHandler(self.access_file)
-		self.out.setFormatter(log_format)
-		self.log.addHandler(self.out)
-
-		# Configs
-		with open(os.environ['VISORHOME']+"/config/fake_log_gen.json") as config_file:
-			config = json.load(config_file)
+class Test_Log_Gen:
 
 	def test_sample(self):
 		x = 'this'
 		assert 'h' in x
 
-	def test_access_log_heartbeat(self):
-		mode = 'access'
-		heart_num = 10
-		log_gen = access_test(self.log, self.config, mode, heart_num)
-		log_gen.run()	
+	def test_access_log_heartbeat(self, tmpdir):
+		#access_file = tmpdir.mkdir("sub").join("access_logs_"+str(datetime.datetime.now())[-6:]+".txt")
+		with open("access_logs_test.txt", "w+") as access_file:
+			# Logs
+			log = logging.getLogger('Gen')
+			logging.basicConfig(level=logging.INFO)
+
+			log_format = logging.Formatter("%(message)s")
+			out = logging.FileHandler("access_logs_test.txt")
+			out.setFormatter(log_format)
+			log.addHandler(out)
+
+		# Configs
+			with open(os.environ['VISORHOME']+"/config/fake_log_gen.json") as config_file:
+				config = json.load(config_file)
+
+
+			mode = 'access'
+			heart_num = 10
+			log_gen = access_test(log, config, mode, heart_num)
+			log_gen.run()	
 
 			
-		assert self.access_file.read() != ''
+			assert access_file.read() != ''
 
 
