4ef27595aa352e2e31ccfa9166dd465f61140de6
xuwenyihust <1016635786@qq.com>
2017-01-15 14:13:11 -0800

    Error logs tested.

--- tests/generator_test.py
+++ tests/generator_test.py
@@ -14,7 +14,7 @@ from src.fake_log_gen import fake_log_gen
 class access_test(fake_log_gen.fake_access_gen):
 
 	def __init__(self, log, config, mode, heart_num, access_num):
-		self.log = log
+		'''self.log = log
 		self.mode = mode
 		# Dict that contains config info
 		self.config = config
@@ -28,6 +28,8 @@ class access_test(fake_log_gen.fake_access_gen):
 		self.codes = self.config["access"]["code"]
 		self.codes_dist = self.config["access"]["code_dist"]
 		self.versions = self.config["access"]["version"]
+		'''
+		super(access_test, self).__init__(log, config, mode)
 
 		# Uniquely in testing
 		self.heart_num = heart_num
@@ -70,9 +72,94 @@ class access_test(fake_log_gen.fake_access_gen):
 			yield from asyncio.sleep(random.uniform(self.access_min, self.access_max))
 
 
+class error_test(fake_log_gen.fake_error_gen):
+	
+	def __init__(self, log, config, mode, heart_num, warn_num, error_num):
+		super(error_test, self).__init__(log, config, mode)
+		self.heat_num = heart_num
+		self.warn_num = warn_num
+		self.error_num = error_num
+
+	def run(self):
+		self.loop = asyncio.get_event_loop()
+		try:
+			self.loop.run_until_complete(
+				asyncio.wait([
+					self.heartbeat_lines(),
+					self.warn_lines(),
+					self.error_lines()]
+				)
+			)
+		finally:
+			self.loop.close()
+
+	@coroutine
+	def heartbeat_lines(self):
+		for i in range(self.heart_num):
+			self.log.info("[-] [-] " + self.config["heartbeat"]["message"])
+			yield from asyncio.sleep(int(self.config["heartbeat"]["interval"]))
+
+	@coroutine
+	def warn_lines(self):
+		for i in range(self.warn_num):
+			pid = ''.join(str(random.randint(0, 9)) for i in range(5))
+			tid = ''.join(str(random.randint(0, 9)) for i in range(10))
+			ip = '.'.join(str(random.randint(0, 255)) for i in range(4))
+			self.log.warning("[pid %s:tid %s] [client %s] %s", pid, tid, ip, self.warnings[random.randrange(len(self.warnings))])
+			yield from asyncio.sleep(random.uniform(self.warn_min, self.warn_max))
+
+	@coroutine
+	def error_lines(self):
+		for i in range(self.error_num):
+			pid = ''.join(str(random.randint(0, 9)) for i in range(5))
+			tid = ''.join(str(random.randint(0, 9)) for i in range(10))
+			ip = '.'.join(str(random.randint(0, 255)) for i in range(4))
+			self.log.error("[pid %s:tid %s] [client %s] %s", pid, tid, ip, self.errors[random.randrange(len(self.errors))])
+			yield from asyncio.sleep(random.uniform(self.error_min, self.error_max))
+
+
+# Testing Class
+
 class Test_Log_Gen:
 
-	def test_access_log_heartbeat(self):	
+	def test_error_log(self):
+		f = os.environ['VISORHOME']+"/tests/tmp/"+"error_logs_test.txt"
+		with open(f, "w+") as error_file:
+			# Logs
+			log = logging.getLogger('Gen')
+			logging.basicConfig(level=logging.INFO)
+
+			log_format = logging.Formatter("%(message)s")
+			out = logging.FileHandler(f)
+			out.setFormatter(log_format)
+			log.addHandler(out)
+
+			# Configs
+			with open(os.environ['VISORHOME']+"/config/fake_log_gen.json") as config_file:
+				config = json.load(config_file)
+
+			mode = 'error'
+			heart_num = 3
+			warn_num = 3
+			error_num = 3
+			log_gen = error_test(log, config, mode, heart_num, warn_num, error_num)
+			log_gen.run()
+
+			sample_line = '127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326'
+			line_size = sys.getsizeof(sample_line)
+
+			lines = access_file.readlines(line_size*100)
+			total_num = len(lines)
+			heart_num_res = len([line for line in lines if 'HEARTBEAT' in line])
+			warn_num_res = len([line for line in lines if 'WARNING' in line])
+			error_num_res = len([line for line in lines if 'ERROR' in line])
+			
+			assert heart_num_res > 0
+			assert warn_num_res > 0
+			assert error_num_res > 0
+
+
+	def test_access_log(self):	
 		f = os.environ['VISORHOME']+"/tests/tmp/"+"access_logs_test.txt"
 		with open(f, "w+") as access_file:
 			# Logs
