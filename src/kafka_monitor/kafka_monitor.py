be295b1d00ad3ddca49f94cb015e8d8c46ba663f
xuwenyihust <1016635786@qq.com>
2017-03-02 14:41:44 -0800

    Added top ip feature.

--- src/kafka_monitor/kafka_monitor.py
+++ src/kafka_monitor/kafka_monitor.py
@@ -32,12 +32,17 @@ class kafka_monitor(object):
 		# Set the summary report params
 		# report interval: in terms of sec
 		self.report_interval = self.config['email']['report']['interval']
-		self.error_cnt = 0
-		self.error_li = []
+		# The batch interval may also be used aside the sparkstreamingcontext creation
+		self.batch_interval = self.config['batch_interval']	
 
 		# Count the total number of logs
 		# Used in summary report system
 		self.total_log_num = 0
+
+		# Pick out the top ips in all logs
+		self.top_ips=[('None', 0) for i in range(3)]
+		# Pick out the top ips in [ERROR] logs
+		self.top_error_ips=[('None', 0) for i in range(3)]
 
 	
 	# Send an email every time detects an error log
@@ -96,23 +101,49 @@ Message: %s
 			TEXT = '''
 Basic:
 
-	Log count: {lc}
+	Log count:
+	- {lc}
 
-	Error count: {ec}
+	Error count:
+	- {ec}
 
-	Error rate: {er: .2f}%
+	Error rate:
+	- {er: .2f}%
 
-Performance:
+Statistics:
 
-	Log import rate: {lr: .2f} per sec
-			'''.format(lc=self.total_log_num, ec=error_cnt, er=error_cnt/self.total_log_num*100, lr=self.total_log_num/self.report_interval)
+	Log import rate:
+	- {lr: .2f} per sec
 
-			#if len(errors) == 0:
-			#	TEXT += 'None'
-			#else:
-			#	for error in errors:
-			#		TEXT += error
+	Top ip addresses:
+	- {top_ip_0}: {top_ip_0_num}
+	- {top_ip_1}: {top_ip_1_num}
+	- {top_ip_2}: {top_ip_2_num}
 
+	Top error ip addresses:
+	- {top_eip_0}: {top_eip_0_num}
+    - {top_eip_1}: {top_eip_1_num}
+    - {top_eip_2}: {top_eip_2_num}
+
+			'''.format(lc = self.total_log_num, \
+					   ec = error_cnt, \
+					   er = error_cnt/self.total_log_num*100, \
+					   lr = self.total_log_num/self.report_interval, \
+					   top_ip_0 = self.top_ips[0][0], \
+					   top_ip_0_num = self.top_ips[0][1], \
+					   top_ip_1 = self.top_ips[1][0], \
+					   top_ip_1_num = self.top_ips[1][1], \
+					   top_ip_2 = self.top_ips[2][0], \
+					   top_ip_2_num = self.top_ips[2][1], \
+					   top_eip_0 = self.top_error_ips[0][0], \
+					   top_eip_0_num = self.top_error_ips[0][1], \
+                       top_eip_1 = self.top_error_ips[1][0], \
+                       top_eip_1_num = self.top_error_ips[1][1], \
+                       top_eip_2 = self.top_error_ips[2][0], \
+                       top_eip_2_num = self.top_error_ips[2][1] \
+					  )
+
+		
 			# Sender Gmail Sign In
 			gmail_sender = self.config_private['email']['address']
 			gmail_passwd = self.config_private['email']['password']
@@ -136,9 +167,16 @@ Performance:
 
 	def run(self):
 		# Consume Kafka streams directly, without receivers
-		lines = KafkaUtils.createDirectStream(self.ssc, [self.topic], {"metadata.broker.list": self.addr})
-		# Extract the log text messages	
+		lines = KafkaUtils.createDirectStream(self.ssc, [self.topic], {"metadata.broker.list": self.addr})	
+		# Performe lines.foreachRDD(lambda x: print(x.collect()))
+		# Get: [(None, '[-] [-] HEARTBEAT\n'), (None, '[-] [-] HEARTBEAT\n'), (None, '[-] [-] HEARTBEAT\n')]	[(None, '[-] [-] HEARTBEAT\n'), (None, '[-] [-] HEARTBEAT\n'), (None, '[-] [-] HEARTBEAT\n')] ...	
+
+		# Extract the valuable log from the tuple, discard 'None'  
 		val_lines = lines.map(lambda x: x[1])
+		val_lines.cache()
+		# Performe val_lines.foreachRDD(lambda x: print('val_lines:', x.collect()))
+		# Get: ['[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n']
+
 		# Filter out the error logs
 		error_lines = val_lines.filter(lambda x: 'ERROR' in x)
 
@@ -148,9 +186,52 @@ Performance:
 		# Use val_sum_lines to store all log lines in the window
 		# val_sum_lines.count() will be used as the total num of logs in the window
 		# Same window size as error_sum_lines, but slides much faster to update faster	
-		val_sum_lines = val_lines.window(self.report_interval, 3)
+		val_sum_lines = val_lines.window(self.report_interval, self.batch_interval)
+		# Performe val_sum_lines.foreachRDD(lambda x: print(x.collect()))
+		# Get: ['[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n', '[-] [-] HEARTBEAT\n' ...]
+
+		# SUM(*) GROUP BY client ip address (from all log lines except HAERTBEAT)
+		# SORT BY SUM(*)
+		val_sum_lines_top_ip = val_sum_lines.filter(lambda x: 'HEARTBEAT' not in x) \
+											.map(lambda x: (x.split(']')[3].lstrip(' ').lstrip('[client '),1)) \
+											.reduceByKey(lambda x, y: x+y) \
+											.map(lambda x: (x[1], x[0])) \
+											.transform(lambda x: x.sortByKey()) \
+											.map(lambda x: (x[1], x[0]))
+
+		# From val_sum_lines_top_ip, get the top 3 client ip addresses
+		# Assign them to List self.top_ips
+		def get_top_ips(val_sum_lines_top_ip):
+			if val_sum_lines_top_ip.count() > 3:
+				self.top_ips = val_sum_lines_top_ip.take(3)
+			else:
+				self.top_ips[:val_sum_lines_top_ip.count()] = val_sum_lines_top_ip.collect()
+			return
+	
+		val_sum_lines_top_ip.foreachRDD(get_top_ips)
+		#val_sum_lines_top_ip.pprint()
+
 		# Window the error lines
 		error_sum_lines = error_lines.window(self.report_interval, self.report_interval)
+
+		# SUM(*) GROUP BY client ip address (from [ERROR] lines)
+		error_sum_lines_top_ip = error_sum_lines.map(lambda x: (x.split(']')[3].lstrip(' ').lstrip('[client '),1)) \
+												.reduceByKey(lambda x, y: x+y) \
+												.map(lambda x: (x[1], x[0])) \
+												.transform(lambda x: x.sortByKey()) \
+												.map(lambda x: (x[1], x[0]))
+
+		# From error_sum_lines_top_ip, get the top 3 client ip addresses
+		# Assign them to List self.top_error_ips
+		def get_top_error_ips(error_sum_lines_top_ip):
+			if error_sum_lines_top_ip.count() > 3:
+				self.top_error_ips = error_sum_lines_top_ip.take(3)
+			else:
+				self.top_error_ips[:error_sum_lines_top_ip.count()] = error_sum_lines_top_ip.collect()
+			return
+
+		error_sum_lines_top_ip.foreachRDD(get_top_error_ips)
+
 		# Generate summary report every window time
 		error_sum_lines.foreachRDD(lambda x: self.summary_report_email(x.collect(), x.count()))
 	
