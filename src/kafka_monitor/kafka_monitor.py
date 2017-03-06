d23b41b1b8a2ea11d41de14023a3cf3a892bb7bd
xuwenyihust <1016635786@qq.com>
2017-03-05 20:26:52 -0800

    Set backpressure.

--- src/kafka_monitor/kafka_monitor.py
+++ src/kafka_monitor/kafka_monitor.py
@@ -20,6 +20,7 @@ class kafka_monitor(object):
 		conf.setMaster(self.config['master_url'])
 		conf.setAppName(self.config['app_name'])
 		#conf.set("spark.cores.max", "2")
+		conf.set("spark.streaming.backpressure.enabled","true")
 		# Initialize a SparkContext
 		sc = SparkContext(conf=conf)
 		# Set the batch interval to be 1 sec
